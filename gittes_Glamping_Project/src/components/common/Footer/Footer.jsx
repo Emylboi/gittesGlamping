e747e366b00d0fcdeee8b6db85aeede7896a423a
Emylboi <emil3c53@mcdmstudent.dk>
2025-01-15 12:51:58 +0100

    save1

--- gittes_Glamping_Project/src/components/common/Footer/Footer.jsx
+++ gittes_Glamping_Project/src/components/common/Footer/Footer.jsx
@@ -1,12 +1,21 @@
+import styles from "./footer.module.css";
+
 const Footer = () => {
   return (
-    <>
-      <footer>
-        <div>
-          <p>Footer</p>
+    <footer>
+      <div className={styles.container}>
+        <div className={styles.inner}>
+          <div className={styles.socials}>
+            <img src="" alt="" /> {/* Facebook */}
+            <img src="" alt="" /> {/* Instagram */}
+          </div>
+          <div className={styles.logoTitle}>
+            <img src="" alt="" /> {/* Logo */}
+            <h1 className={styles.title}>Gittes Glamping</h1>
+          </div>
         </div>
-      </footer>
-    </>
+      </div>
+    </footer>
   );
 };
 
